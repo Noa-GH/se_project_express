a4fb257d2163ef8868f69d8086f5cb69397d56b7
Noa-GH <fordnt17@gmail.com>
2026-05-04 21:47:53 -0400

    Chore: Temp placement of hardcoded user id GitHub Actions test phase.

--- app.js
+++ app.js
@@ -18,6 +18,14 @@ mongoose
 app.use(cors());
 app.use(express.json());
 
+app.use((req, res, next) => {
+  req.user = {
+    _id: "5d8b8592978f8bd833c8d133",
+  };
+
+  next();
+});
+
 app.use("/", router);
 
 app.listen(PORT, () => {
